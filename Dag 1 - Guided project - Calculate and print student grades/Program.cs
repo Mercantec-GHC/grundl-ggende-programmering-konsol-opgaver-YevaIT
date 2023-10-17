910dae477f828c71f577d10f6e14a91142a33ccb
Yevheniia Zhygalova <119417215+YevaIT@users.noreply.github.com>
2023-10-17 13:28:41 +0200

    Task 1.2

--- Dag 1 - Guided project - Calculate and print student grades/Program.cs
+++ Dag 1 - Guided project - Calculate and print student grades/Program.cs
@@ -24,3 +24,23 @@ int jeong2 = 92;
 int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
+
+float sophia = (sophia1 + sophia2 + sophia3 + sophia4 + sophia5) / currentAssignments;
+
+Console.WriteLine(sophia);
+float AddUp(params int[] values)
+{
+    int sum = 0;
+    foreach (int value in values)
+    {
+        sum += value;
+    }
+    float result = sum / currentAssignments;
+    return result;
+}
+
+Console.WriteLine(AddUp(sophia1, sophia2, sophia3, sophia4, sophia5));
+Console.WriteLine(AddUp(nicolas1, nicolas2, nicolas3, nicolas4, nicolas5));
+Console.WriteLine(AddUp(zahirah1, zahirah2, zahirah3, zahirah4, zahirah5));
+Console.WriteLine(AddUp(jeong1, jeong2, jeong3, jeong4, jeong5));
+
