c330f80f2e9e88b576f7666eb0d6267cb600df75
Yevheniia Zhygalova <119417215+YevaIT@users.noreply.github.com>
2023-10-18 12:42:18 +0200

    solve Day2

--- Dag 2 - Guided project - Develop foreach/Program.cs
+++ Dag 2 - Guided project - Develop foreach/Program.cs
@@ -9,11 +9,16 @@ int sophia3 = 87;
 int sophia4 = 98;
 int sophia5 = 100;
 
+int[] sophia_arr = new int[] {sophia1, sophia2, sophia3, sophia4, sophia5};
+
 int andrew1 = 92;
 int andrew2 = 89;
 int andrew3 = 81;
 int andrew4 = 96;
 int andrew5 = 90;
+
+int[] andrew_arr = new int[] {andrew1, andrew2, andrew3, andrew4, andrew5};
+
 
 int emma1 = 90;
 int emma2 = 85;
@@ -21,31 +26,36 @@ int emma3 = 87;
 int emma4 = 98;
 int emma5 = 68;
 
+int[] emma_arr = new int[] {emma1, emma2, emma3, emma4, emma5 };
+
+
 int logan1 = 90;
 int logan2 = 95;
 int logan3 = 87;
 int logan4 = 88;
 int logan5 = 96;
 
-int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
+int[] logan_arr = new int[] { logan1, logan2, logan3, logan4, logan5};
 
-decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
 
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
 
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
+
+decimal sophiaScore = AddUp(sophia_arr);
+decimal andrewScore = AddUp(andrew_arr);
+decimal emmaScore = AddUp(emma_arr);
+decimal loganScore = AddUp(logan_arr);
+
+decimal AddUp(params int[] values)
+{
+    int sum = 0;
+    foreach (int value in values)
+    {
+        sum += value;
+    }
+    decimal result = sum / currentAssignments;
+    return result;
+}
+
 
 Console.WriteLine("Student\t\tGrade\n");
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
